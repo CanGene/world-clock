8069b8aafa7aadcd5f625c68cb6edb3533180ab9
Candace Genesis <candace.genesis@gmail.com>
2025-10-23 11:32:00 -0700

    Added the functionality for the dropdown to select a city and display the time

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,49 @@
 function updateLive() {
   let vancouverElement = document.querySelector("#vancouver");
-  let vancouverDateElement = vancouverElement.querySelector(".date");
-  let vancouverTimeElement = vancouverElement.querySelector(".time");
-  let vancouverTimeZone = moment().tz("America/Vancouver");
+  if (vancouverElement) {
+    let vancouverDateElement = vancouverElement.querySelector(".date");
+    let vancouverTimeElement = vancouverElement.querySelector(".time");
+    let vancouverTimeZone = moment().tz("America/Vancouver");
 
-  vancouverDateElement.innerHTML = vancouverTimeZone.format("MMMM Do YYYY");
-  vancouverTimeElement.innerHTML = vancouverTimeZone.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    vancouverDateElement.innerHTML = vancouverTimeZone.format("MMMM Do YYYY");
+    vancouverTimeElement.innerHTML = vancouverTimeZone.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let muscatElement = document.querySelector("#muscat");
-  let muscatDateElement = muscatElement.querySelector(".date");
-  let muscatTimeElement = muscatElement.querySelector(".time");
-  let muscatTimeZone = moment().tz("Asia/Muscat");
+  if (muscatElement) {
+    let muscatDateElement = muscatElement.querySelector(".date");
+    let muscatTimeElement = muscatElement.querySelector(".time");
+    let muscatTimeZone = moment().tz("Asia/Muscat");
 
-  muscatDateElement.innerHTML = muscatTimeZone.format("MMMM Do YYYY");
-  muscatTimeElement.innerHTML = muscatTimeZone.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    muscatDateElement.innerHTML = muscatTimeZone.format("MMMM Do YYYY");
+    muscatTimeElement.innerHTML = muscatTimeZone.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 updateLive();
 setInterval(updateLive, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
+  let cityTime = moment().tz(cityTimeZone);
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div>`;
+}
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
